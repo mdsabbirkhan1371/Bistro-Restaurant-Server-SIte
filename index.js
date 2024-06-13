c278e40630022254c4c77bd56e87ddb59f859412
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-06-13 12:34:25 +0600

    updated menu feature

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function run() {
 
     // middlewares for verify token second stem jwt
     const verifyToken = (req, res, next) => {
-      console.log('inside verify token', req.headers.authorization);
+      // console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: 'Unauthorized Access' });
       }
@@ -159,6 +159,43 @@ async function run() {
       const menuItem = req.body;
       console.log(menuItem);
       const result = await menuCollection.insertOne(menuItem);
+      res.send(result);
+    });
+    // update data
+
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch('/menu/:id', async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image,
+        },
+      };
+
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // delete item form db menu
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      console.log('delete req id ', id);
+      const query = { _id: new ObjectId(id) };
+
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     });
 
